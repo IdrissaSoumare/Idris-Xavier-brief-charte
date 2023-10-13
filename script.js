d65e2c0692ba2b65dc30582f1de079f28450fd13
Xavier OILLIC <xavieroillic@gmail.com>
2023-10-13 16:00:15 +0200

    modif' xav

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-var acc = document.getElementsByClassName("accordion");
+var accordeon = document.getElementsByClassName("accordion");
 var i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+for (i = 0; i < accordeon.length; i++) {
+  accordeon[i].addEventListener("click", function() {
     /* Toggle between adding and removing the "active" class,
     to highlight the button that controls the panel */
     this.classList.toggle("active");
@@ -16,3 +16,128 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+
+var boutonBas = document.getElementsByid("change");
+var j;
+var tab = [];
+
+for (i = 0; j <= tab.length - 1; j++) {
+    random[j].addEventlistener("click", function () {
+    
+    tab[0] = 0;
+    tab[1] = 1;
+    tab[2] = 2;
+    tab[3] = 3;
+    tab[4] = 4;
+    tab[5] = 5;
+    tab[6] = 6;
+    tab[7] = 7;
+    tab[8] = 8;
+    tab[9] = 9;
+        tab[j] = Math.floor(Math.random() * tab.length);
+        if (tab[j] == 0) {
+            "Kris";
+        } else {
+            if (tab[j] == 1) {
+                "Audrey";
+            } else {
+                if (tab[j] == 2) {
+                    "Manue";
+                } else {
+                    if (tab[j] == 3) {
+                        "Severine";
+                    } else {
+                        if (tab[j] == 4) {
+                            "Elodie";
+                        } else {
+                            if (tab[j] == 5) {
+                                "Xavier";
+                            } else {
+                                if (tab[j] == 6) {
+                                   "VIrand";
+                                } else {
+                                    if (tab[j] == 7) {
+                                        "Martin";
+                                    } else {
+                                        if (tab[j] == 8) {
+                                            "Davy";
+                                        } else {
+                                            if (tab[j] == 9) {
+                                                "Idris";
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    })
+}
+
+
+/*function main() {
+    random[j].addEventlistener("click" function () {
+    var boutonBas = document.getElementsByClassName("bouton_rouge_bas");
+    var j;
+    var tab = createArray(10);
+
+    tab[0] = 0;
+    tab[1] = 1;
+    tab[2] = 2;
+    tab[3] = 3;
+    tab[4] = 4;
+    tab[5] = 5;
+    tab[6] = 6;
+    tab[7] = 7;
+    tab[8] = 8;
+    tab[9] = 9;
+
+    for (i = 0; j <= tab.length - 1; j++) {
+        tab[i] = Math.floor(Math.random() * tab.length);
+        if (tab[j] == 0) {
+            window.alert("Kris");
+        } else {
+            if (tab[j] == 1) {
+                window.alert("Audrey");
+            } else {
+                if (tab[j] == 2) {
+                    window.alert("Manue");
+                } else {
+                    if (tab[j] == 3) {
+                        window.alert("Severine");
+                    } else {
+                        if (tab[j] == 4) {
+                            window.alert("Elodie");
+                        } else {
+                            if (tab[j] == 5) {
+                                window.alert("Xavier");
+                            } else {
+                                if (tab[j] == 6) {
+                                    window.alert("VIrand");
+                                } else {
+                                    if (tab[j] == 7) {
+                                        window.alert("Martin");
+                                    } else {
+                                        if (tab[j] == 8) {
+                                            window.alert("Davy");
+                                        } else {
+                                            if (tab[j] == 9) {
+                                                window.alert("Idris");
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+})
+main();
+*/
