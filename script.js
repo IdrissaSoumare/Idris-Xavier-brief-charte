e3130f3e98fb4f81cd0989fd63e4805f8bb45535
Xavier OILLIC <xavieroillic@gmail.com>
2023-10-13 17:44:16 +0200

    On y est presque

--- script.js
+++ script.js
@@ -17,14 +17,17 @@ for (i = 0; i < accordeon.length; i++) {
   });
 }
 
+//----------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------
 
-var boutonBas = document.getElementsByid("change");
-var j;
-var tab = [];
+const boutonBas = document.querySelector('#change')
+boutonBas.addEventListener('click', function(){
+    /*boutonBas.innerText = "COUCOU !";*/
+    /*console.log(boutonBas) ICI, on remplace RANDOM APPRENANT par COUCOU et on le log.*/
+    var j;
+    var tab = [];
 
-for (i = 0; j <= tab.length - 1; j++) {
-    random[j].addEventlistener("click", function () {
-    
     tab[0] = 0;
     tab[1] = 1;
     tab[2] = 2;
@@ -37,96 +40,34 @@ for (i = 0; j <= tab.length - 1; j++) {
     tab[9] = 9;
         tab[j] = Math.floor(Math.random() * tab.length);
         if (tab[j] == 0) {
-            "Kris";
+           prompt("Kriss");
         } else {
             if (tab[j] == 1) {
-                "Audrey";
+                prompt ("Audrey");
             } else {
                 if (tab[j] == 2) {
-                    "Manue";
+                    prompt ("Manue");
                 } else {
                     if (tab[j] == 3) {
-                        "Severine";
+                        prompt("Severine");
                     } else {
                         if (tab[j] == 4) {
-                            "Elodie";
+                           prompt("Elodie");
                         } else {
                             if (tab[j] == 5) {
-                                "Xavier";
+                                prompt("Xavier");
                             } else {
                                 if (tab[j] == 6) {
-                                   "VIrand";
+                                  prompt("VIrand");
                                 } else {
                                     if (tab[j] == 7) {
-                                        "Martin";
+                                        prompt("Martin");
                                     } else {
                                         if (tab[j] == 8) {
-                                            "Davy";
+                                           prompt("Davy");
                                         } else {
                                             if (tab[j] == 9) {
-                                                "Idris";
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    })
-}
-
-
-/*function main() {
-    random[j].addEventlistener("click" function () {
-    var boutonBas = document.getElementsByClassName("bouton_rouge_bas");
-    var j;
-    var tab = createArray(10);
-
-    tab[0] = 0;
-    tab[1] = 1;
-    tab[2] = 2;
-    tab[3] = 3;
-    tab[4] = 4;
-    tab[5] = 5;
-    tab[6] = 6;
-    tab[7] = 7;
-    tab[8] = 8;
-    tab[9] = 9;
-
-    for (i = 0; j <= tab.length - 1; j++) {
-        tab[i] = Math.floor(Math.random() * tab.length);
-        if (tab[j] == 0) {
-            window.alert("Kris");
-        } else {
-            if (tab[j] == 1) {
-                window.alert("Audrey");
-            } else {
-                if (tab[j] == 2) {
-                    window.alert("Manue");
-                } else {
-                    if (tab[j] == 3) {
-                        window.alert("Severine");
-                    } else {
-                        if (tab[j] == 4) {
-                            window.alert("Elodie");
-                        } else {
-                            if (tab[j] == 5) {
-                                window.alert("Xavier");
-                            } else {
-                                if (tab[j] == 6) {
-                                    window.alert("VIrand");
-                                } else {
-                                    if (tab[j] == 7) {
-                                        window.alert("Martin");
-                                    } else {
-                                        if (tab[j] == 8) {
-                                            window.alert("Davy");
-                                        } else {
-                                            if (tab[j] == 9) {
-                                                window.alert("Idris");
+                                               prompt("Idris");
                                             }
                                         }
                                     }
@@ -138,6 +79,5 @@ for (i = 0; j <= tab.length - 1; j++) {
             }
         }
     }
-})
-main();
-*/
+    /*boutonBas.innerText = tab[j]*/
+    )
