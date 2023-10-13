bfb6f2a2cff7a62caca6afdbb27124a8e4ee957a
Xavier OILLIC <xavieroillic@gmail.com>
2023-10-13 18:35:44 +0200

    Finish

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var accordeon = document.getElementsByClassName("accordion");
 var i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
+for (i = 0; i < accordeon.length; i++) {
+  accordeon[i].addEventListener("click", function () {
     /* Toggle between adding and removing the "active" class,
     to highlight the button that controls the panel */
     this.classList.toggle("active");
@@ -23,8 +23,8 @@ for (i = 0; i < acc.length; i++) {
 
 const boutonBas = document.querySelector('#change')
 boutonBas.addEventListener('click', function(){
-    /*boutonBas.innerText = "COUCOU !";*/
-    /*console.log(boutonBas) ICI, on remplace RANDOM APPRENANT par COUCOU et on le log.*/
+    //boutonBas.innerText = "COUCOU !";
+    //console.log(boutonBas) ICI, on remplace RANDOM APPRENANT par COUCOU et on le log.
     var j;
     var tab = [];
 
@@ -40,34 +40,34 @@ boutonBas.addEventListener('click', function(){
     tab[9] = 9;
         tab[j] = Math.floor(Math.random() * tab.length);
         if (tab[j] == 0) {
-           prompt("Kriss");
+           boutonBas.innerText = "Kriss";
         } else {
             if (tab[j] == 1) {
-                prompt ("Audrey");
+                boutonBas.innerText ="Audrey";
             } else {
                 if (tab[j] == 2) {
-                    prompt ("Manue");
+                    boutonBas.innerText ="Manue";
                 } else {
                     if (tab[j] == 3) {
-                        prompt("Severine");
+                        boutonBas.innerText ="Severine";
                     } else {
                         if (tab[j] == 4) {
-                           prompt("Elodie");
+                           boutonBas.innerText ="Elodie";
                         } else {
                             if (tab[j] == 5) {
-                                prompt("Xavier");
+                                boutonBas.innerText ="Xavier";
                             } else {
                                 if (tab[j] == 6) {
-                                  prompt("VIrand");
+                                  boutonBas.innerText ="VIrand";
                                 } else {
                                     if (tab[j] == 7) {
-                                        prompt("Martin");
+                                        boutonBas.innerText ="Martin";
                                     } else {
                                         if (tab[j] == 8) {
-                                           prompt("Davy");
+                                           boutonBas.innerText ="Davy";
                                         } else {
                                             if (tab[j] == 9) {
-                                               prompt("Idris");
+                                               boutonBas.innerText ="Idriss";
                                             }
                                         }
                                     }
@@ -78,6 +78,70 @@ boutonBas.addEventListener('click', function(){
                 }
             }
         }
-    }
-    /*boutonBas.innerText = tab[j]*/
-    )
+    })
+
+//----------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------
+//----------------------CODE VALIDE mais a simplifier !---------------------------------------------------------
+
+/*const boutonBas = document.querySelector('#change')
+boutonBas.addEventListener('click', function(){
+    //boutonBas.innerText = "COUCOU !";
+    //console.log(boutonBas) ICI, on remplace RANDOM APPRENANT par COUCOU et on le log.
+    var j;
+    var tab = [];
+
+    tab[0] = 0;
+    tab[1] = 1;
+    tab[2] = 2;
+    tab[3] = 3;
+    tab[4] = 4;
+    tab[5] = 5;
+    tab[6] = 6;
+    tab[7] = 7;
+    tab[8] = 8;
+    tab[9] = 9;
+        tab[j] = Math.floor(Math.random() * tab.length);
+        if (tab[j] == 0) {
+           boutonBas.innerText = "Kriss";
+        } else {
+            if (tab[j] == 1) {
+                boutonBas.innerText ="Audrey";
+            } else {
+                if (tab[j] == 2) {
+                    boutonBas.innerText ="Manue";
+                } else {
+                    if (tab[j] == 3) {
+                        boutonBas.innerText ="Severine";
+                    } else {
+                        if (tab[j] == 4) {
+                           boutonBas.innerText ="Elodie";
+                        } else {
+                            if (tab[j] == 5) {
+                                boutonBas.innerText ="Xavier";
+                            } else {
+                                if (tab[j] == 6) {
+                                  boutonBas.innerText ="VIrand";
+                                } else {
+                                    if (tab[j] == 7) {
+                                        boutonBas.innerText ="Martin";
+                                    } else {
+                                        if (tab[j] == 8) {
+                                           boutonBas.innerText ="Davy";
+                                        } else {
+                                            if (tab[j] == 9) {
+                                               boutonBas.innerText ="Idriss";
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    })
+    */
+//----------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------
