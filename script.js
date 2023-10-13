f1e22eef6aa30c588ac45173029f4c4132e233ee
idris <soumare.id@laposte.net>
2023-10-13 17:31:08 +0200

    bouton du bas

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+  acc[i].addEventListener("click", function () {
     /* Toggle between adding and removing the "active" class,
     to highlight the button that controls the panel */
     this.classList.toggle("active");
@@ -16,3 +16,73 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+function main() {
+  var tab = [];
+
+  tab[0] = 0;
+  tab[1] = 1;
+  tab[2] = 2;
+  tab[3] = 3;
+  tab[4] = 4;
+  tab[5] = 5;
+  tab[6] = 6;
+  tab[7] = 7;
+  tab[8] = 8;
+  tab[9] = 9;
+  var j;
+
+  for (j = 0; j <= tab.length - 1; j++) {
+    tab[j] = Math.floor(Math.random()*tab.length);
+    if (tab[j] == 0) {
+      ("Kris");
+      window.alert("Kris");
+    } else {
+      if (tab[j] == 1) {
+        ("Audrey");
+        window.alert("Audrey");
+      } else {
+        if (tab[j] == 2) {
+          ("Manue");
+          window.alert("Manue");
+        } else {
+          if (tab[j] == 3) {
+            ("Severine");
+            window.alert("Severine");
+          } else {
+            if (tab[j] == 4) {
+              ("Elodie");
+              window.alert("Elodie");
+            } else {
+              if (tab[j] == 5) {
+                ("Xavier");
+                window.alert("Xavier");
+              } else {
+                if (tab[j] == 6) {
+                  window.alert("VIrand");
+                } else {
+                  if (tab[j] == 7) {
+                    window.alert("Martin");
+                  } else {
+                    if (tab[j] == 8) {
+                      window.alert("Davy");
+                    } else {
+                      if (tab[j] == 9) {
+                        window.alert("Idris");
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    document.getElementById("change").button.innerHTML = replace(
+      "Random Apprenant",
+      tab[j]
+    );
+  }
+}
+main();
